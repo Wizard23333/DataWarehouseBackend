dc89a33f450e5b2bc07729c3b9c0d5558d19bd41
Wizard23333 <3233990645@qq.com>
2021-12-03 10:43:38 +0800

    项目初始化

---
+++ src/main/java/com/datawarehousebackend/DataWarehouseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.datawarehousebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataWarehouseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataWarehouseBackendApplication.class, args);
+    }
+
+}
