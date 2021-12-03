352b5c38ad9ffd36df2c14659b12c1daae2bbcab
Wizard23333 <3233990645@qq.com>
2021-12-03 10:43:38 +0800

    项目初始化

---
+++ src/test/java/com/datawarehousebackend/DataWarehouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datawarehousebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataWarehouseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
