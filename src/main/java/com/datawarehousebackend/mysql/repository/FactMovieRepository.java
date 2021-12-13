e60ebfc37ab87ed5535c961275bbffe29601b03f
Wizard23333 <3233990645@qq.com>
2021-12-13 19:04:14 +0800

    几个分数接口；springboot版本更改

--- src/main/java/com/datawarehousebackend/mysql/repository/FactMovieRepository.java
+++ src/main/java/com/datawarehousebackend/mysql/repository/FactMovieRepository.java
@@ -1,11 +1,12 @@
 package com.datawarehousebackend.mysql.repository;
 
+import com.datawarehousebackend.mysql.controller.MovieController;
 import com.datawarehousebackend.mysql.model.FactMovieEntity;
+import io.swagger.models.auth.In;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Map;
@@ -13,12 +14,18 @@ import java.util.Map;
 public interface FactMovieRepository extends JpaRepository<FactMovieEntity, String>, JpaSpecificationExecutor<FactMovieEntity> {
     @Override
     List<FactMovieEntity> findAllById(Iterable<String> strings);
+
     // 通过asin查找
     FactMovieEntity findByAsin(String asin);
+
+    List<FactMovieEntity> findByStyleKey(Integer styleKey);
+
     // 通过timeKey列表查找
     List<FactMovieEntity> findAllByTimeKey(Integer timeKey);
+
     // 通过电影名称来查找 精确匹配
     List<FactMovieEntity> findAllByTitle(String title);
+
     // 通过电影名模糊查询
     List<FactMovieEntity> findAllByTitleContaining(String title);
 
@@ -27,6 +34,13 @@ public interface FactMovieRepository extends JpaRepository<FactMovieEntity, Stri
 
     @Query(nativeQuery = true, value = "select count(asin) from fact_movie where (select sum(helpful_num) from div_review where asin = fact_movie.asin) >= :helpfulNum")
     Integer findMovieNumByHelpfulNum(@Param("helpfulNum") Integer helpfulNum);
+
+    @Query("select asin from FactMovieEntity where timeKey = :timeKey and styleKey = :styleKey")
+    List<String> findAsinByTimeKeyAndStyleKey(@Param("timeKey") Integer timeKey, @Param("styleKey") Integer styleKey);
+
+    @Query("select avg(imdbScore) as avgImdb, avg(reviewPoint) as avgReviewPoint from FactMovieEntity where styleKey = :styleKey")
+    Map<String, Object> findAvgImdbScoreAndReviewPoint(@Param("styleKey") Integer styleKey);
+
 
 
 }
